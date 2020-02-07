897302b03e35a28ad6c60e52633a9e109ed6ac84
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-02-07 18:45:55 -0300

    script for testing

---
+++ tools/hashstr.py
@@ -0,0 +1,9 @@
+from sys import path, argv
+path.append('../source/')
+from hashcracker import hash_password
+
+if len(argv) == 1:
+    print('Usage: <type> <string>')
+    exit()
+
+print(hash_password(argv[2], argv[1]))
